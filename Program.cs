a179b8efa9a3b6f5f30bbb7795fd422a8e3b2392
komal <komal@LAPTOP-GPSG7DIK>
2019-05-27 00:42:55 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace positive_and_negative_program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                int num;
+                Console.Write("\n\n");
+                Console.Write("Check whether a number is positive or negative:\n");
+                Console.Write("----------------------------------------------");
+                Console.Write("\n\n");
+                Console.Write("Input an integer : ");
+                num = Convert.ToInt32(Console.ReadLine());
+                if (num >= 0)
+
+                    Console.WriteLine("{0} is a positive number.\n", num);
+                else
+                    Console.WriteLine("{0} is a negative number. \n", num);
+            }
+        }
+
+    }
+}
